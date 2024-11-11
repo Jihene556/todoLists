4318c8d43ff6402edc49bc42c88ded2df162596b
“jihene” <jihene.haj-hamouda@imt-atlantique.net>
2024-11-11 17:03:02 +0100

    quick fix

--- src/routes/lists/index.ts
+++ src/routes/lists/index.ts
@@ -4,9 +4,6 @@ import * as listsController from '../../controllers/listsController'
 async function lists(fastify: FastifyInstance) {
 
   fastify.get('/', listsController.listLists)
-
-  // TODO implement addList in controller
-  // fastify.post('/', listsController.addList)
   fastify.post('/', listsController.addList);
   fastify.put('/:id', listsController.updateList);
   fastify.post('/:id/items', listsController.addItemToList)
